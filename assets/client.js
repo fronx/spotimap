fecfce7143cdcc52011bbe35bbc3251dbe5355fe
fronx <fronx@wurmus.de>
2022-01-28 20:52:42 +0100

    volume .25 "

--- assets/client.js
+++ assets/client.js
@@ -7,7 +7,7 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
                 document.getElementById('preview-source').setAttribute('src', url)
                 var player = document.getElementById('preview-player');
                 player.load();
-                player.volume = 0.15;
+                player.volume = 0.25;
                 player.play();
                 console.log(JSON.stringify(hoverData.points[0].customdata));
             }
